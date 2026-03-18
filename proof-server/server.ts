15adc5cb4491478ace7bd91c43a9848df38ff072
Satyam Bansal <satyamsgsits1994@gmail.com>
2026-03-18 15:40:02 +0530

    tsconfig changes

--- proof-server/server.ts
+++ proof-server/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import cors from 'cors';
 import * as path from 'path';
 import * as fs from 'fs';
